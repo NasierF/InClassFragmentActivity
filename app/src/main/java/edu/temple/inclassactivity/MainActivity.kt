e8b12563b0b97f7dcc4f3d49417078b6b3aa0129
NasierF <119966231+NasierF@users.noreply.github.com>
2024-10-15 12:02:51 -0400

    committing progress

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -8,12 +8,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         // Fetch images into IntArray called imageArray
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
         val imageArray = IntArray(typedArray.length()) {typedArray.getResourceId(it, 0)}
         typedArray.recycle()
 
         // Attach an instance of ImageDisplayFragment using factory method
+        //fragment instance
+        val fragment1 = ImageDisplayFragment.newInstance(imageArray)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainerView, fragment1)
+            .commit()
 
     }
 }
